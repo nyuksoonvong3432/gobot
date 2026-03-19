b39b87f69542674bd6df0ec1ab29a7c8533af440
Vong Nyuk Soon <nyuksoon.vong@habitap.app>
2026-03-19 17:35:49 +0800

    wip

--- main.go
+++ main.go
@@ -12,41 +12,58 @@ const (
 	Vip
 )
 
+type orderStatus int
+
+const (
+	Idle orderStatus = iota
+	Processing
+	Completed
+)
+
 type order struct {
 	id        int
 	orderType orderType
+	status    orderStatus
 }
 
 type app struct {
+	botId               int
 	botCount            int
 	processSecondPerBot time.Duration
 }
 
 func main() {
 	app := app{
+		botId:               1,
 		botCount:            5,
 		processSecondPerBot: time.Second * 10,
 	}
 
+	results := make(chan order)
 	orders := make(chan order)
-	app.subscribe(orders)
+	app.subscribe(orders, results)
 
-	orders <- order{id: 1, orderType: Normal}
-	orders <- order{id: 2, orderType: Normal}
-	orders <- order{id: 3, orderType: Normal}
+	orders <- order{id: 1, orderType: Normal, status: Idle}
+	orders <- order{id: 2, orderType: Normal, status: Idle}
+	orders <- order{id: 3, orderType: Normal, status: Idle}
 	close(orders)
+
+	<-results
 }
 
-func (a *app) subscribe(orders <-chan order) {
+func (a *app) subscribe(orders chan order, results chan order) {
 	go func() {
 		for o := range orders {
-			log.Printf("Received order id: %d", o.id)
-
+			go a.assignOrderToBot(&o, results)
+			a.botId++
 		}
 	}()
 }
 
-func bot(id int, orders chan<- order) {
-	log.Println("Bot ", id)
-	time.Sleep(10)
+func (a *app) assignOrderToBot(o *order, results chan<- order) {
+	log.Printf("Processing order %d with bot %v ...", o.id, a.botId)
+	time.Sleep(a.processSecondPerBot)
+	log.Printf("Finished processing order %d with bot %v", o.id, a.botId)
+	o.status = Completed
+	results <- *o
 }
