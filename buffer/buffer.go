acbdfe9c5d5a47b2dcbfdf6e87468792cbbbedc8
Vong Nyuk Soon <nyuksoon.vong@habitap.app>
2026-03-20 15:15:33 +0800

    buffer

---
+++ buffer/buffer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	messages := make(chan string, 1)
+
+	messages <- "buffered"
+	fmt.Println(<-messages)
+
+	messages <- "channel"
+	fmt.Println(<-messages)
+}
