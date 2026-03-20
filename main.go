b83a701a70a9597505e2b3b250d5d1c2f3d3876b
Vong Nyuk Soon <nyuksoon.vong@habitap.app>
2026-03-20 09:06:24 +0800

    wip

--- main.go
+++ main.go
@@ -101,6 +101,10 @@ func (a *app) start(normalOrders <-chan order, vipOrders <-chan order, wg *sync.
 
 func bot(id int, normalOrders <-chan order, vipOrders <-chan order, processSecond time.Duration) {
 	for {
+		if normalOrders == nil && vipOrders == nil {
+			break // all channels closed
+		}
+
 		select {
 		case o, more := <-vipOrders:
 			if !more {
